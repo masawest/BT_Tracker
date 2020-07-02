3e39f0c6d076f55e1fdc72a15bbd7135250a723d
masawest <nishi.masayuki@kuas.ac.jp>
2020-07-02 15:08:33 +0900

    Homework 5.3: extended ReminderBroadCastReceiver to startActivity directly (I also took Back Stack into account)

--- app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
@@ -1,5 +1,7 @@
 package com.example.bttracker;
 
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,12 +15,24 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+
+        // ↓　Create an explicit intent for LogActivity
+        Intent toLog = new Intent(context, LogActivity.class);
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(toLog);
+        // バックスタック全体を含むPendingIntentを取得する
+        PendingIntent pendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new
                 NotificationCompat.Builder(context, "BT Tracker Channel")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        // ↓下記の意味は、Set the intent that will fire when the user taps the notification.
+                .setContentIntent(pendingIntent);
         // ↑上記の".setSmallIcon"などは、"NotificationCompat.Builder"の部分を省略していて、
         // たとえば、".setSmallIcon"は、NotificationCompat.Builder.setSmallIconである。たぶん。
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
