a6e1ac67aa0944815b1bc4a5624b4755cc0b26dc
masawest <nishi.masayuki@kuas.ac.jp>
2020-07-08 22:23:40 +0900

    Homework 6.1: styled up Log-, Normal-, Mechanism-Activities.

--- app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
@@ -17,6 +17,17 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
         // an Intent broadcast.
 
         // ↓　Create an explicit intent for LogActivity
+        // ↓ 以下のIntent(context, LogActivity.class)について、これはディベロッパーのサイトを
+        // を見て自分で書いたのだが、最初"context"ではなく、"this"と書いた。しかしエラーがでた。
+        // そこで、この後にでてくるコードの中のここから5行目に
+        // NotificationCompat.Builder(context, "BT Tracker Channel")というのがあったので、
+        // もしかして、と思い、"context"を入れたところうまく動くことがわかった。このことにについて
+        // 先生に質問したところ、以下の回答があり、なるほど、と思った！
+        // In Android apps, there are different ways to get context: within an Activity itself
+        // use "this"; for BroadcastReceiver use the receiver's own context.
+        // In line 15 (直前のコード), did you see
+        // onReceive(Context context, Intent intent) <-- that "context" is the receiver's
+        // own context.
         Intent toLog = new Intent(context, LogActivity.class);
         // Create the TaskStackBuilder and add the intent, which inflates the back stack
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
